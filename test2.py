5adf8318aff5ccd8d018864c01cf5003ce41e369
Shivam <Shivamkumar188132@gmail.com>
2024-03-25 15:49:25 +0530

    first commit

---
+++ test2.py
@@ -0,0 +1,5 @@
+import pandas as pd
+test_df = pd.DataFrame({'Age': ['Young', 'Young', 'Young', 'Young', 'Old', 'Old', 'Old', 'Old'],
+                        'Actor': ['Jack', 'Arnold', 'Keanu', 'Sylvester', 'Jack', 'Arnold', 'Keanu', 'Sylvester'],
+                        'Power': [100, 80, 25, 50, 99, 75, 5, 30]})
+print(test_df)
