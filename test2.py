c608573d1e08b820ff0b6ea9caddbcf9fa39aeef
shivam188132 <shivamkumar188132@gmail.com>
2025-05-12 22:19:55 +0530

    10:19 PM may 12

--- test2.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-test_df = pd.DataFrame({'Age': ['Young', 'Young', 'Young', 'Young', 'Old', 'Old', 'Old', 'Old'],
-                        'Actor': ['Jack', 'Arnold', 'Keanu', 'Sylvester', 'Jack', 'Arnold', 'Keanu', 'Sylvester'],
-                        'Power': [100, 80, 25, 50, 99, 75, 5, 30]})
-print(test_df)
-pivoted_df = test_df.pivot(index='Age', columns='Actor', values='Power')
-print(pivoted_df)
